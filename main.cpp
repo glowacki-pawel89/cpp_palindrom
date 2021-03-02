d5004a419b51b11b6bb7e9c3dae7c54bebfbe597
ser-dziadula <54303115+ser-dziadula@users.noreply.github.com>
2021-03-02 18:32:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
 
-std::string isPalindrome(std::string word) {
+bool isPalindrome(const std::string & word)
+{
     std::string reversed = std::string(word.rbegin(), word.rend());
-    return word == reversed ? "Wprowadzone slowo jest palindromem" : "Wprowadzone slowo nie jest palindromem";
+    return word == reversed;
+}
+
+std::string makeLowerWithoutSpaces(const std::string & base)
+{
+    std::string lowered;
+    for(auto l: base) {
+        if(l != ' ') {
+            lowered += std::tolower(l);  
+        }
+    }
+    return lowered;
 }
 
 int main()
 {
     std::string input;
-    std::cout << "Wprowadz slowo\n";
-    std::cin >> input;
-    std::cout << isPalindrome(input);
+    std::string modifiedInput;
+    std::cout << "Wprowadz zdanie\n";
+    std::getline(std::cin, input);
+    modifiedInput = makeLowerWithoutSpaces(input);
+    if(isPalindrome(modifiedInput))
+    {
+        std::cout << "Wprowadzone slowo jest palindromem";
+    }
+    else
+    {
+        std::cout << "Wprowadzone slowo nie jest palindromem";
+    }
     return 0;
 }
